044352735ef65bfb55281725a1086d40c605b4ce
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2022-08-14 17:38:25 +0700

    mini project BE-clothes-stores

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const swaggerUi = require("swagger-ui-express"),
+    swaggerDocument = require("./swagger.json");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use("/public", express.static("public"));
 
 app.use(routes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`Clothes Stores App listening on port ${port}`);
